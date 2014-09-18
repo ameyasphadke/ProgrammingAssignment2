068f0e6b165ff007a62e16e5ae3b030c9d264178
ameyasphadke <ameya.phadke@gmail.com>
2014-09-18 23:04:39 +0200

    initial draft

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,21 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function returns special matrix, which overrides existing matrix behavior
+## The new matrix has setter/getters for matrix and for inverse of the matrix - via set/get
+## solve methods
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        matrix(set = set, get = get,
+               setsolve = setsolve,
+               getsolve = getsolve)
 
 }
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
