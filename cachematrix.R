9207a52340b8a5d28638deb8ee0fd42f7537b48b
ameyasphadke <ameya.phadke@gmail.com>
2014-09-21 11:22:42 +0200

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,22 +3,28 @@
 ## solve methods
 makeCacheMatrix <- function(x = matrix(numeric())) {
         m <- NULL
+        
         ## sets new matrix and nullifies its inverse
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
-        ## gets
+        ## returns the original matrix i.e. matrix x
         get <- function() x
+        
+        ## "Wraper" method for solve. Returns inverse if available in cache or calls 
+        ## solve method to compute inverse
         setsolve <- function(solve) m <<- solve
+        
+        ##  Returns inverse of the matrix i.e. matrix m
         getsolve <- function() m
+        ## Finally rwturn list of methods
         list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 
 }
 
-
-## Write a short comment describing this function
-
+## This function calls required methos to retrieve inverse of the matrix 
+## from cache - if available or calls solve and sets the result in cache for future use
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getsolve()
@@ -26,6 +32,7 @@ cacheSolve <- function(x, ...) {
                 message("getting cached data")
                 return(m)
         }
+        
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)
